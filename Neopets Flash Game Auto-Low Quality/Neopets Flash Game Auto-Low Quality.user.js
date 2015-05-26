e5f5a33bf773aae8b1f77b24b6c10154be6eb8da
demeiz <medievalduel@gmail.com>
2015-05-26 22:31:04 +0100

    Update Neopets Flash Game Auto-Low Quality.user.js

--- Neopets Flash Game Auto-Low Quality/Neopets Flash Game Auto-Low Quality.user.js
+++ Neopets Flash Game Auto-Low Quality/Neopets Flash Game Auto-Low Quality.user.js
@@ -2,14 +2,17 @@
 // @name        Neopets Flash Game Auto-Low Quality
 // @namespace   Jarofgrease.captainmaxthecat.com
 // @description Automatically sets the quality for flash games to low in the settings
-// @author 		Demeiz
-// @email 		admin@captainmaxthecat.com
+// @author 	Demeiz
+// @email 	admin@captainmaxthecat.com
 // @homepage	http://www.captainmaxthecat.com
-// @version		1.0
+// @version	0.2
 // @language	en
-// @match       http://www.neopets.com/games/game.phtml?game_id=*
-// @require		http://code.jquery.com/jquery-latest.min.js
-// @grant		none
+// @include     *neopets.com/games/game.phtml?game_id=*
+// @exclude     *neopets.com/games/game.phtml?game_id=*&size=regular&quality=low&play=true
+// @require	http://code.jquery.com/jquery-latest.min.js
+// @grant	none
 // ==/UserScript==
 
+// set the hidden game quality value as low
 $("input:hidden#sel_qual_dd").val("low");
+// current issue with this method is that the menu is no longer accessible.
